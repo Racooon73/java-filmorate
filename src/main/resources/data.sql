7d4129ec37019da26ca2cdd0f6f402849fb41782
Max Dezhnev <mvdezhnyov@stud.etu.ru>
2022-10-27 01:38:08 +0300

    feat: добавить работу с БД

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,15 @@
+DELETE from FRIENDS;
+DELETE  FROM USERS ;
+
+DELETE FROM likes;
+
+DELETE FROM FILM_GENRE;
+DELETE FROM FILMS;
+DELETE FROM RATINGS;
+
+ALTER TABLE FILMS ALTER COLUMN id RESTART WITH 1;
+
+ALTER TABLE USERS ALTER COLUMN id RESTART WITH 1;
 merge into RATINGS (RATING_ID, RATING_NAME) values (1,'G' ),(2,'PG'),(3,'PG-13'),(4,'M'),(5, 'NC-17');
 merge into GENRE (GENRE_ID,GENRE_NAME) values ( 1,'Комедия'), (2,'Драма'),(3,'Мультфильм'),( 4,'Триллер'),
                                               (5,'Ужасы'),(6,'Документальный');
